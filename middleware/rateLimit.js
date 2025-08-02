160c52fc0da537a9650821cbb42ec66d314677a1
Nityananda-Krishnamoorthy <contactmenitish06@gmail.com>
2025-08-03 00:32:56 +0530

    rateLimit-Update

--- middleware/rateLimit.js
+++ middleware/rateLimit.js
@@ -2,11 +2,11 @@
 const rateLimit = require('express-rate-limit');
 
 const authLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, 
-  max: 5,
+  windowMs: 2 * 60 * 1000, 
+  max: 10,
   message: {
     status: "fail",
-    message: "Too many attempts, please try again after 15 minutes",
+    message: "Too many attempts, please try again after 2 minutes",
   },
   standardHeaders: true, // for better client-side handling
   legacyHeaders: false,
